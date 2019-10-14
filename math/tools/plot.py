0a51e6450706c11889adf639496a6acedea00fd9
Szabolcs Nagy <szabolcs.nagy@arm.com>
2019-10-14 11:37:10 +0100

    math: update the plot script
    Fix it to be python3 compatible and plot the exact and approximated
    values too.

--- math/tools/plot.py
+++ math/tools/plot.py
@@ -18,39 +18,44 @@ def fhex(s):
 
 def parse(f):
 	xs = []
+	gs = []
 	ys = []
 	es = []
 	# Has to match the format used in ulp.c
-	r = re.compile(r'[^ (]+\(([^ )]*)\) got [^ ]+ want ([^ ]+) [^ ]+ ulp err ([^ ]+)')
+	r = re.compile(r'[^ (]+\(([^ )]*)\) got ([^ ]+) want ([^ ]+) [^ ]+ ulp err ([^ ]+)')
 	for line in f:
 		m = r.match(line)
 		if m:
 			x = fhex(m.group(1))
-			y = fhex(m.group(2))
-			e = float(m.group(3))
+			g = fhex(m.group(2))
+			y = fhex(m.group(3))
+			e = float(m.group(4))
 			xs.append(x)
+			gs.append(g)
 			ys.append(y)
 			es.append(e)
 		elif line.startswith('PASS') or line.startswith('FAIL'):
 			# Print the summary line
 			print(line)
-	return xs, ys, es
+	return xs, gs, ys, es
 
-def plot(xs, ys, es):
+def plot(xs, gs, ys, es):
 	if len(xs) < 2:
 		print('not enough samples')
 		return
 	a = min(xs)
 	b = max(xs)
 	fig, (ax0,ax1) = plt.subplots(nrows=2)
-	es = map(abs, es) # ignore the sign
+	es = np.abs(es) # ignore the sign
 	emax = max(es)
 	ax0.text(a+(b-a)*0.7, emax*0.8, '%s\n%g'%(emax.hex(),emax))
-	ax0.plot(xs,es,'rx')
+	ax0.plot(xs,es,'r.')
 	ax0.grid()
-	ax1.plot(xs,ys,'rx')
+	ax1.plot(xs,ys,'r.',label='want')
+	ax1.plot(xs,gs,'b.',label='got')
 	ax1.grid()
+	ax1.legend()
 	plt.show()
 
-xs, ys, es = parse(sys.stdin)
-plot(xs, ys, es)
+xs, gs, ys, es = parse(sys.stdin)
+plot(xs, gs, ys, es)
