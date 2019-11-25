6d3ae5fc1a64ec195f22a48bc51658cff7cf1cb3
Krzysztof Koch <krzysztof.koch@arm.com>
2019-11-25 10:08:08 +0000

    arch64: Add SIMD version of memcpy
    Create a new memcpy implementation for targets with the NEON extension.
    
    __memcpy_aarch64_simd has been tested on a range of modern
    microarchitectures. It turned out to be faster than __memcpy_aarch64 on
    all of them, with a performance improvement of 3-11% depending on the
    platform.

--- string/include/stringlib.h
+++ string/include/stringlib.h
@@ -26,6 +26,9 @@ char *__strchrnul_aarch64 (const char *, int );
 size_t __strlen_aarch64 (const char *);
 size_t __strnlen_aarch64 (const char *, size_t);
 int __strncmp_aarch64 (const char *, const char *, size_t);
+#if __ARM_NEON
+void *__memcpy_aarch64_simd (void *__restrict, const void *__restrict, size_t);
+#endif
 # if __ARM_FEATURE_SVE
 void *__memchr_aarch64_sve (const void *, int, size_t);
 int __memcmp_aarch64_sve (const void *, const void *, size_t);
--- string/test/memcpy.c
+++ string/test/memcpy.c
@@ -21,6 +21,9 @@ F(memcpy)
 #if __aarch64__
 F(__memcpy_bytewise)
 F(__memcpy_aarch64)
+# if __ARM_NEON
+F(__memcpy_aarch64_simd)
+# endif
 #elif __arm__
 F(__memcpy_arm)
 #endif
