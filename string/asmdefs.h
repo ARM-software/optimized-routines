232e3c08c9eb9aba822510bae57f920351b1eb40
Tamas Zsoldos <tamas.zsoldos@arm.com>
2020-04-28 17:13:10 +0100

    string: ARMv8.5 BTI: Add BTI support to assembly files.
    This change addds the landing pads to the start of functions
    implemented in assembly, by adding it to the ENTRY macro. To avoid
    skipping it when using an alias, every ENTRY_ALIAS use must precede
    the corresponding ENTRY.
    
    Furthermore, the GNU property note is added to the assembly
    files. Since none of the functions save LR to stack, both BTI and PAC
    support are indicated.
    
    Paddings before __strncmp_aarch64 and __strnlen_aarch64 were adjusted.

--- string/asmdefs.h
+++ string/asmdefs.h
@@ -8,12 +8,53 @@
 #ifndef _ASMDEFS_H
 #define _ASMDEFS_H
 
+#if defined(__aarch64__)
+
+/* Branch Target Identitication support.  */
+#define BTI_C		hint	34
+#define BTI_J		hint	36
+/* Return address signing support (pac-ret).  */
+#define PACIASP		hint	25; .cfi_window_save
+#define AUTIASP		hint	29; .cfi_window_save
+
+#define FEATURE_1_BTI 1
+#define FEATURE_1_PAC 2
+
+/* Add a GNU_PROPERTY_AARCH64_FEATURE_1_AND note.  */
+#define GNU_PROPERTY(features)		\
+  .section .note.gnu.property, "a";	\
+  .p2align 3;				\
+  .word 4;				\
+  .word 16;				\
+  .word 5;				\
+  .asciz "GNU";				\
+  .word 0xc0000000;			\
+  .word 4;				\
+  .word features;			\
+  .word 0;
+
+#define END_FILE GNU_PROPERTY(FEATURE_1_BTI|FEATURE_1_PAC)
+
+#define ENTRY_ALIGN(name, alignment)	\
+  .global name;		\
+  .type name,%function;	\
+  .align alignment;		\
+  name:			\
+  .cfi_startproc;	\
+  BTI_C;
+
+#else
+
+#define END_FILE
+
 #define ENTRY_ALIGN(name, alignment)	\
   .global name;		\
   .type name,%function;	\
   .align alignment;		\
   name:			\
   .cfi_startproc;
+
+#endif
 
 #define ENTRY(name)	ENTRY_ALIGN(name, 6)
 
