e75171004368758406f78d08db56072b334dbbaa
Wilco Dijkstra <wilco.dijkstra@arm.com>
2020-05-12 11:59:16 +0100

    string: Cleanup strnlen test
    Improve strnlen test coverage and cleanup code.

--- string/test/strnlen.c
+++ string/test/strnlen.c
@@ -1,7 +1,7 @@
 /*
  * strnlen test.
  *
- * Copyright (c) 2019, Arm Limited.
+ * Copyright (c) 2019-2020, Arm Limited.
  * SPDX-License-Identifier: MIT
  */
 
@@ -35,19 +35,18 @@ static const struct fun
 };
 #undef F
 
-#define A 32
-#define SP 512
-#define LEN 250000
-static char sbuf[LEN + 2 * A + 1];
+#define ALIGN 32
+#define LEN 512
+static char sbuf[LEN + 3 * ALIGN];
 
 static void *
 alignup (void *p)
 {
-  return (void *) (((uintptr_t) p + A - 1) & -A);
+  return (void *) (((uintptr_t) p + ALIGN - 1) & -ALIGN);
 }
 
 static void
-test (const struct fun *fun, int align, int maxlen, int len)
+test (const struct fun *fun, int align, size_t maxlen, size_t len)
 {
   char *src = alignup (sbuf);
   char *s = src + align;
@@ -56,44 +55,42 @@ test (const struct fun *fun, int align, int maxlen, int len)
 
   if (err_count >= ERR_LIMIT)
     return;
-  if (len > LEN || align >= A)
+  if (len > LEN || align > ALIGN)
     abort ();
 
-  for (int i = 0; i < len + A; i++)
-    src[i] = '?';
+  for (int i = 0; src + i < s; i++)
+    src[i] = 0;
+  for (int i = 1; i <= ALIGN; i++)
+    s[len + i] = 0;
   for (int i = 0; i < len; i++)
-    s[i] = 'a' + i % 23;
-  s[len] = '\0';
+    s[i] = 'a' + (i & 31);
+  s[len] = 0;
+  s[((len ^ align) & 1) ? e + 1 : len + 1] = 0;
 
   r = fun->fun (s, maxlen);
   if (r != e)
     {
-      ERR ("%s(%p, %d) returned %zu\n", fun->name, s, maxlen, r);
-      quote ("input", src, len + A);
-      printf ("expected: %zu\n", e);
+      ERR ("%s (%p, %zu) len %zu returned %zu, expected %zu\n", fun->name, s,
+	   maxlen, len, r, e);
+      quote ("input", s, len);
     }
 }
 
 int
-main ()
+main (void)
 {
   int r = 0;
   for (int i = 0; funtab[i].name; i++)
     {
       err_count = 0;
-      for (int a = 0; a < A; a++)
-	{
-	  int n;
-	  for (n = 1; n < 100; n++)
-	    for (int maxlen = 0; maxlen < 100; maxlen++)
+      for (int a = 0; a < ALIGN; a++)
+	for (int n = 0; n < LEN; n++)
+	  {
+	    for (int maxlen = 0; maxlen < LEN; maxlen++)
 	      test (funtab + i, a, maxlen, n);
-	  for (; n < LEN; n *= 2)
-	    {
-	      test (funtab + i, a, n * 2, n);
-	      test (funtab + i, a, n, n);
-	      test (funtab + i, a, n / 2, n);
-	    }
-	}
+	    test (funtab + i, a, SIZE_MAX - a, n);
+	  }
+
       printf ("%s %s\n", err_count ? "FAIL" : "PASS", funtab[i].name);
       if (err_count)
 	r = -1;
