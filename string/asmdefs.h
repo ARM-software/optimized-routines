ARM-software/optimized-routines

commit 40b662ce7b65d5eaefa40fd8046d6f3c6b3238c1

string: add .fnstart and .fnend directives to ENTRY/END macros
Modify the ENTRY_ALIGN and END assembler macros to mark the start and
end of functions for arm unwind tables.

Enables the pacbti epilogue function to emit .save{} directives for
stack unwinding.

string/asmdefs.h
@@ -8,6 +8,11 @@
 #ifndef _ASMDEFS_H
 #define _ASMDEFS_H
 
+#if defined (__arm__)
+#define ARM_FNSTART .fnstart
+#define ARM_FNEND .fnend
+#endif
+
 #if defined(__aarch64__)
 
 /* Branch Target Identitication support.  */
@@ -52,6 +57,7 @@ GNU_PROPERTY (FEATURE_1_AND, FEATURE_1_BTI|FEATURE_1_PAC)
   .type name,%function;	\
   .align alignment;		\
   name:			\
+  ARM_FNSTART;		\
   .cfi_startproc;	\
   BTI_C;
 
@@ -64,6 +70,7 @@ GNU_PROPERTY (FEATURE_1_AND, FEATURE_1_BTI|FEATURE_1_PAC)
   .type name,%function;	\
   .align alignment;		\
   name:			\
+  ARM_FNSTART;		\
   .cfi_startproc;
 
 #endif
@@ -77,6 +84,7 @@ GNU_PROPERTY (FEATURE_1_AND, FEATURE_1_BTI|FEATURE_1_PAC)
 
 #define END(name)	\
   .cfi_endproc;		\
+  ARM_FNEND;		\
   .size name, .-name;
 
 #define L(l) .L ## l

commit e373f6595230087a8ddea449bfb14b47150b4059

string: Fix ARM_FNSTART on non-arm targets
Fix build failure introduced by

  commit 40b662ce7b65d5eaefa40fd8046d6f3c6b3238c1
  string: add .fnstart and .fnend directives to ENTRY/END macros

string/asmdefs.h
@@ -11,6 +11,9 @@
 #if defined (__arm__)
 #define ARM_FNSTART .fnstart
 #define ARM_FNEND .fnend
+#else
+#define ARM_FNSTART
+#define ARM_FNEND
 #endif
 
 #if defined(__aarch64__)