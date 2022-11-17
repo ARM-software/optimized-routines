67a49eeffa7bd4d90a4edf54cb9d4d784ab170a7
Szabolcs Nagy <szabolcs.nagy@arm.com>
2022-11-17 11:13:22 +0000

    string: Add separate asmdefs.h per target
    The definitions in this header are necessarily target specific, so
    better to have a separate version in each target directory.

--- string/arm/asmdefs.h
+++ string/arm/asmdefs.h
@@ -1,14 +1,12 @@
 /*
- * Macros for asm code.
+ * Macros for asm code.  Arm version.
  *
- * Copyright (c) 2019-2020, Arm Limited.
+ * Copyright (c) 2019-2022, Arm Limited.
  * SPDX-License-Identifier: MIT OR Apache-2.0 WITH LLVM-exception
  */
 
 #ifndef _ASMDEFS_H
 #define _ASMDEFS_H
-
-#if defined (__arm__)
 
 #define ARM_FNSTART .fnstart
 #if defined (IS_LEAF)
@@ -456,64 +454,6 @@
 	.endif
 .endm
 
-#else /* !defined (__arm__) */
-
-#define ARM_FNSTART
-#define ARM_FNEND
-
-#endif
-
-#if defined(__aarch64__)
-
-/* Branch Target Identitication support.  */
-#define BTI_C		hint	34
-#define BTI_J		hint	36
-/* Return address signing support (pac-ret).  */
-#define PACIASP		hint	25; .cfi_window_save
-#define AUTIASP		hint	29; .cfi_window_save
-
-/* GNU_PROPERTY_AARCH64_* macros from elf.h.  */
-#define FEATURE_1_AND 0xc0000000
-#define FEATURE_1_BTI 1
-#define FEATURE_1_PAC 2
-
-/* Add a NT_GNU_PROPERTY_TYPE_0 note.  */
-#define GNU_PROPERTY(type, value)	\
-  .section .note.gnu.property, "a";	\
-  .p2align 3;				\
-  .word 4;				\
-  .word 16;				\
-  .word 5;				\
-  .asciz "GNU";				\
-  .word type;				\
-  .word 4;				\
-  .word value;				\
-  .word 0;				\
-  .text
-
-/* If set then the GNU Property Note section will be added to
-   mark objects to support BTI and PAC-RET.  */
-#ifndef WANT_GNU_PROPERTY
-#define WANT_GNU_PROPERTY 1
-#endif
-
-#if WANT_GNU_PROPERTY
-/* Add property note with supported features to all asm files.  */
-GNU_PROPERTY (FEATURE_1_AND, FEATURE_1_BTI|FEATURE_1_PAC)
-#endif
-
-#define ENTRY_ALIGN(name, alignment)	\
-  .global name;		\
-  .type name,%function;	\
-  .align alignment;		\
-  name:			\
-  ARM_FNSTART;		\
-  .cfi_startproc;	\
-  BTI_C;
-
-#else
-
-#define END_FILE
 
 #define ENTRY_ALIGN(name, alignment)	\
   .global name;		\
@@ -522,8 +462,6 @@ GNU_PROPERTY (FEATURE_1_AND, FEATURE_1_BTI|FEATURE_1_PAC)
   name:			\
   ARM_FNSTART;		\
   .cfi_startproc;
-
-#endif
 
 #define ENTRY(name)	ENTRY_ALIGN(name, 6)
 
@@ -538,19 +476,5 @@ GNU_PROPERTY (FEATURE_1_AND, FEATURE_1_BTI|FEATURE_1_PAC)
   .size name, .-name;
 
 #define L(l) .L ## l
-
-#ifdef __ILP32__
-  /* Sanitize padding bits of pointer arguments as per aapcs64 */
-#define PTR_ARG(n)  mov w##n, w##n
-#else
-#define PTR_ARG(n)
-#endif
-
-#ifdef __ILP32__
-  /* Sanitize padding bits of size arguments as per aapcs64 */
-#define SIZE_ARG(n)  mov w##n, w##n
-#else
-#define SIZE_ARG(n)
-#endif
 
 #endif
