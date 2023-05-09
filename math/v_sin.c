45c437680055ea7061e1204193851b80c799c82e
Joe Ramsay <Joe.Ramsay@arm.com>
2023-05-09 09:41:45 +0100

    math: Cleanup Neon sin and sinf
    Replace implicit arithmetic operations with intrinsics and move data
    to struct for improved memory access.

--- math/v_sin.c
+++ math/v_sin.c
@@ -4,46 +4,39 @@
  * Copyright (c) 2019-2023, Arm Limited.
  * SPDX-License-Identifier: MIT OR Apache-2.0 WITH LLVM-exception
  */
-
 #include "mathlib.h"
 #include "v_math.h"
 
-static const double Poly[] = {
-/* worst-case error is 3.5 ulp.
-   abs error: 0x1.be222a58p-53 in [-pi/2, pi/2].  */
--0x1.9f4a9c8b21dc9p-41,
- 0x1.60e88a10163f2p-33,
--0x1.ae6361b7254e7p-26,
- 0x1.71de382e8d62bp-19,
--0x1.a01a019aeb4ffp-13,
- 0x1.111111110b25ep-7,
--0x1.55555555554c3p-3,
+static const volatile struct __v_sin_data
+{
+  float64x2_t poly[7];
+  float64x2_t inv_pi, pi_1, pi_2, pi_3, shift;
+} data = {
+  .poly = {/* worst-case error is 3.5 ulp.
+	      abs error: 0x1.be222a58p-53 in [-pi/2, pi/2].  */
+	   V2 (-0x1.55555555554c3p-3), V2 (0x1.111111110b25ep-7),
+	   V2 (-0x1.a01a019aeb4ffp-13), V2 (0x1.71de382e8d62bp-19),
+	   V2 (-0x1.ae6361b7254e7p-26), V2 (0x1.60e88a10163f2p-33),
+	   V2 (-0x1.9f4a9c8b21dc9p-41)},
+
+  .inv_pi = V2 (0x1.45f306dc9c883p-2),
+  .pi_1 = V2 (0x1.921fb54442d18p+1),
+  .pi_2 = V2 (0x1.1a62633145c06p-53),
+  .pi_3 = V2 (0x1.c1cd129024e09p-106),
+  .shift = V2 (0x1.8p52),
 };
 
-#define C7 v_f64 (Poly[0])
-#define C6 v_f64 (Poly[1])
-#define C5 v_f64 (Poly[2])
-#define C4 v_f64 (Poly[3])
-#define C3 v_f64 (Poly[4])
-#define C2 v_f64 (Poly[5])
-#define C1 v_f64 (Poly[6])
-
-#define InvPi v_f64 (0x1.45f306dc9c883p-2)
-#define Pi1 v_f64 (0x1.921fb54442d18p+1)
-#define Pi2 v_f64 (0x1.1a62633145c06p-53)
-#define Pi3 v_f64 (0x1.c1cd129024e09p-106)
-#define Shift v_f64 (0x1.8p52)
-#define AbsMask v_u64 (0x7fffffffffffffff)
-
 #if WANT_SIMD_EXCEPT
-#define TinyBound 0x202 /* top12 (asuint64 (0x1p-509)).  */
-#define Thresh 0x214	/* top12 (asuint64 (RangeVal)) - TinyBound.  */
+#define TinyBound v_u64 (0x2020000000000000) /* asuint64 (0x1p-509).  */
+#define Thresh v_u64 (0x2140000000000000)    /* RangeVal - TinyBound.  */
 #else
-#define RangeVal v_f64 (0x1p23)
+#define RangeVal v_u64 (0x4160000000000000) /* asuint64(0x1p23).  */
 #endif
 
+#define C(i) data.poly[i]
+
 static float64x2_t VPCS_ATTR NOINLINE
-specialcase (float64x2_t x, float64x2_t y, uint64x2_t cmp)
+special_case (float64x2_t x, float64x2_t y, uint64x2_t cmp)
 {
   return v_call_f64 (sin, x, y, cmp);
 }
@@ -53,46 +46,47 @@ float64x2_t VPCS_ATTR V_NAME (sin) (float64x2_t x)
   float64x2_t n, r, r2, y;
   uint64x2_t sign, odd, cmp, ir;
 
-  ir = vreinterpretq_u64_f64 (x) & AbsMask;
-  r = vreinterpretq_f64_u64 (ir);
-  sign = vreinterpretq_u64_f64 (x) & ~AbsMask;
+  r = vabsq_f64 (x);
+  ir = vreinterpretq_u64_f64 (r);
+  sign = veorq_u64 (ir, vreinterpretq_u64_f64 (x));
 
 #if WANT_SIMD_EXCEPT
   /* Detect |x| <= 0x1p-509 or |x| >= RangeVal. If fenv exceptions are to be
      triggered correctly, set any special lanes to 1 (which is neutral w.r.t.
-     fenv). These lanes will be fixed by specialcase later.  */
-  cmp = (ir >> 52) - TinyBound >= Thresh;
+     fenv). These lanes will be fixed by special-case handler later.  */
+  cmp = vcgeq_u64 (vsubq_u64 (ir, TinyBound), Thresh);
   if (unlikely (v_any_u64 (cmp)))
     r = vbslq_f64 (cmp, v_f64 (1), r);
 #else
-  cmp = ir >= vreinterpretq_u64_f64 (RangeVal);
+  cmp = vcgeq_u64 (ir, RangeVal);
 #endif
 
   /* n = rint(|x|/pi).  */
-  n = vfmaq_f64 (Shift, InvPi, r);
-  odd = vreinterpretq_u64_f64 (n) << 63;
-  n -= Shift;
+  n = vfmaq_f64 (data.shift, data.inv_pi, r);
+  odd = vshlq_n_u64 (vreinterpretq_u64_f64 (n), 63);
+  n = vsubq_f64 (n, data.shift);
 
   /* r = |x| - n*pi  (range reduction into -pi/2 .. pi/2).  */
-  r = vfmaq_f64 (r, -Pi1, n);
-  r = vfmaq_f64 (r, -Pi2, n);
-  r = vfmaq_f64 (r, -Pi3, n);
+  r = vfmsq_f64 (r, data.pi_1, n);
+  r = vfmsq_f64 (r, data.pi_2, n);
+  r = vfmsq_f64 (r, data.pi_3, n);
 
   /* sin(r) poly approx.  */
-  r2 = r * r;
-  y = vfmaq_f64 (C6, C7, r2);
-  y = vfmaq_f64 (C5, y, r2);
-  y = vfmaq_f64 (C4, y, r2);
-  y = vfmaq_f64 (C3, y, r2);
-  y = vfmaq_f64 (C2, y, r2);
-  y = vfmaq_f64 (C1, y, r2);
-  y = vfmaq_f64 (r, y * r2, r);
+  r2 = vmulq_f64 (r, r);
+  y = vfmaq_f64 (C (5), C (6), r2);
+  y = vfmaq_f64 (C (4), y, r2);
+  y = vfmaq_f64 (C (3), y, r2);
+  y = vfmaq_f64 (C (2), y, r2);
+  y = vfmaq_f64 (C (1), y, r2);
+  y = vfmaq_f64 (C (0), y, r2);
+  y = vfmaq_f64 (r, vmulq_f64 (y, r2), r);
 
   /* sign.  */
-  y = vreinterpretq_f64_u64 (vreinterpretq_u64_f64 (y) ^ sign ^ odd);
+  y = vreinterpretq_f64_u64 (
+    veorq_u64 (veorq_u64 (vreinterpretq_u64_f64 (y), sign), odd));
 
   if (unlikely (v_any_u64 (cmp)))
-    return specialcase (x, y, cmp);
+    return special_case (x, y, cmp);
   return y;
 }
 VPCS_ALIAS
--- math/v_sinf.c
+++ math/v_sinf.c
@@ -8,28 +8,34 @@
 #include "mathlib.h"
 #include "v_math.h"
 
-static const float Poly[] = {
-  /* 1.886 ulp error */
-  0x1.5b2e76p-19f,
-  -0x1.9f42eap-13f,
-  0x1.110df4p-7f,
-  -0x1.555548p-3f,
+static const volatile struct __v_sinf_data
+{
+  float32x4_t poly[4];
+  float32x4_t pi_1, pi_2, pi_3, inv_pi, shift;
+} data = {
+  .poly = {/* 1.886 ulp error.  */
+	   V4 (-0x1.555548p-3f), V4 (0x1.110df4p-7f),
+	   V4 (-0x1.9f42eap-13f), V4 (0x1.5b2e76p-19f)},
+
+  .pi_1 = V4 (0x1.921fb6p+1f),
+  .pi_2 = V4 (-0x1.777a5cp-24f),
+  .pi_3 = V4 (-0x1.ee59dap-49f),
+
+  .inv_pi = V4 (0x1.45f306p-2f),
+  .shift = V4 (0x1.8p+23f),
 };
-#define Pi1 v_f32 (0x1.921fb6p+1f)
-#define Pi2 v_f32 (-0x1.777a5cp-24f)
-#define Pi3 v_f32 (-0x1.ee59dap-49f)
-#define A3 v_f32 (Poly[3])
-#define A5 v_f32 (Poly[2])
-#define A7 v_f32 (Poly[1])
-#define A9 v_f32 (Poly[0])
-#define RangeVal v_f32 (0x1p20f)
-#define TinyBound v_f32 (0x1p-61f)
-#define InvPi v_f32 (0x1.45f306p-2f)
-#define Shift v_f32 (0x1.8p+23f)
-#define AbsMask v_u32 (0x7fffffff)
+
+#if WANT_SIMD_EXCEPT
+#define TinyBound v_u32 (0x21000000) /* asuint32(0x1p-61f).  */
+#define Thresh v_u32 (0x28800000)    /* RangeVal - TinyBound.  */
+#else
+#define RangeVal v_u32 (0x49800000) /* asuint32(0x1p20f).  */
+#endif
+
+#define C(i) data.poly[i]
 
 static float32x4_t VPCS_ATTR NOINLINE
-specialcase (float32x4_t x, float32x4_t y, uint32x4_t cmp)
+special_case (float32x4_t x, float32x4_t y, uint32x4_t cmp)
 {
   /* Fall back to scalar code.  */
   return v_call_f32 (sinf, x, y, cmp);
@@ -40,45 +46,44 @@ float32x4_t VPCS_ATTR V_NAME (sinf) (float32x4_t x)
   float32x4_t n, r, r2, y;
   uint32x4_t sign, odd, cmp, ir;
 
-  ir = vreinterpretq_u32_f32 (x) & AbsMask;
-  r = vreinterpretq_f32_u32 (ir);
-  sign = vreinterpretq_u32_f32 (x) & ~AbsMask;
+  r = vabsq_f32 (x);
+  ir = vreinterpretq_u32_f32 (r);
+  sign = veorq_u32 (ir, vreinterpretq_u32_f32 (x));
 
 #if WANT_SIMD_EXCEPT
-  cmp
-    = (ir - vreinterpretq_u32_f32 (TinyBound)
-       >= vreinterpretq_u32_f32 (RangeVal) - vreinterpretq_u32_f32 (TinyBound));
+  cmp = vcgeq_u32 (vsubq_u32 (ir, TinyBound), Thresh);
   if (unlikely (v_any_u32 (cmp)))
     /* If fenv exceptions are to be triggered correctly, set any special lanes
        to 1 (which is neutral w.r.t. fenv). These lanes will be fixed by
-       specialcase later.  */
+       special-case handler later.  */
     r = vbslq_f32 (cmp, v_f32 (1), r);
 #else
-  cmp = ir >= vreinterpretq_u32_f32 (RangeVal);
+  cmp = vcgeq_u32 (ir, RangeVal);
 #endif
 
   /* n = rint(|x|/pi) */
-  n = vfmaq_f32 (Shift, InvPi, r);
-  odd = vreinterpretq_u32_f32 (n) << 31;
-  n -= Shift;
+  n = vfmaq_f32 (data.shift, data.inv_pi, r);
+  odd = vshlq_n_u32 (vreinterpretq_u32_f32 (n), 31);
+  n = vsubq_f32 (n, data.shift);
 
   /* r = |x| - n*pi  (range reduction into -pi/2 .. pi/2) */
-  r = vfmaq_f32 (r, -Pi1, n);
-  r = vfmaq_f32 (r, -Pi2, n);
-  r = vfmaq_f32 (r, -Pi3, n);
+  r = vfmsq_f32 (r, data.pi_1, n);
+  r = vfmsq_f32 (r, data.pi_2, n);
+  r = vfmsq_f32 (r, data.pi_3, n);
 
   /* y = sin(r) */
-  r2 = r * r;
-  y = vfmaq_f32 (A7, A9, r2);
-  y = vfmaq_f32 (A5, y, r2);
-  y = vfmaq_f32 (A3, y, r2);
-  y = vfmaq_f32 (r, y * r2, r);
+  r2 = vmulq_f32 (r, r);
+  y = vfmaq_f32 (C (2), C (3), r2);
+  y = vfmaq_f32 (C (1), y, r2);
+  y = vfmaq_f32 (C (0), y, r2);
+  y = vfmaq_f32 (r, vmulq_f32 (y, r2), r);
 
   /* sign fix */
-  y = vreinterpretq_f32_u32 (vreinterpretq_u32_f32 (y) ^ sign ^ odd);
+  y = vreinterpretq_f32_u32 (
+    veorq_u32 (veorq_u32 (vreinterpretq_u32_f32 (y), sign), odd));
 
   if (unlikely (v_any_u32 (cmp)))
-    return specialcase (x, y, cmp);
+    return special_case (x, y, cmp);
   return y;
 }
 VPCS_ALIAS
