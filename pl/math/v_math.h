ff8527ecdb08e28a785616d881f919c91f9200f1
Joe Ramsay <Joe.Ramsay@arm.com>
2023-06-16 10:08:57 +0100

    pl/math: Remove unused v_math helpers
    Unused by any routine in PL.

--- pl/math/v_math.h
+++ pl/math/v_math.h
@@ -23,89 +23,6 @@
 
 # include <stdint.h>
 # include "math_config.h"
-
-/* reinterpret as type1 from type2.  */
-static inline uint32_t
-as_u32_f32 (float x)
-{
-  union
-  {
-    float f;
-    uint32_t u;
-  } r = { x };
-  return r.u;
-}
-static inline float
-as_f32_u32 (uint32_t x)
-{
-  union
-  {
-    uint32_t u;
-    float f;
-  } r = { x };
-  return r.f;
-}
-static inline int32_t
-as_s32_u32 (uint32_t x)
-{
-  union
-  {
-    uint32_t u;
-    int32_t i;
-  } r = { x };
-  return r.i;
-}
-static inline uint32_t
-as_u32_s32 (int32_t x)
-{
-  union
-  {
-    int32_t i;
-    uint32_t u;
-  } r = { x };
-  return r.u;
-}
-static inline uint64_t
-as_u64_f64 (double x)
-{
-  union
-  {
-    double f;
-    uint64_t u;
-  } r = { x };
-  return r.u;
-}
-static inline double
-as_f64_u64 (uint64_t x)
-{
-  union
-  {
-    uint64_t u;
-    double f;
-  } r = { x };
-  return r.f;
-}
-static inline int64_t
-as_s64_u64 (uint64_t x)
-{
-  union
-  {
-    uint64_t u;
-    int64_t i;
-  } r = { x };
-  return r.i;
-}
-static inline uint64_t
-as_u64_s64 (int64_t x)
-{
-  union
-  {
-    int64_t i;
-    uint64_t u;
-  } r = { x };
-  return r.u;
-}
-
 # if __aarch64__
 
 #  include <arm_neon.h>
@@ -121,12 +38,6 @@ as_u64_s64 (int64_t x)
 	x, x, x, x                                                            \
       }
 
-static inline int
-v_lanes32 (void)
-{
-  return 4;
-}
-
 static inline float32x4_t
 v_f32 (float x)
 {
@@ -141,38 +52,6 @@ static inline int32x4_t
 v_s32 (int32_t x)
 {
   return (int32x4_t) V4 (x);
-}
-
-static inline float
-v_get_f32 (float32x4_t x, int i)
-{
-  return x[i];
-}
-static inline uint32_t
-v_get_u32 (uint32x4_t x, int i)
-{
-  return x[i];
-}
-static inline int32_t
-v_get_s32 (int32x4_t x, int i)
-{
-  return x[i];
-}
-
-static inline void
-v_set_f32 (float32x4_t *x, int i, float v)
-{
-  (*x)[i] = v;
-}
-static inline void
-v_set_u32 (uint32x4_t *x, int i, uint32_t v)
-{
-  (*x)[i] = v;
-}
-static inline void
-v_set_s32 (int32x4_t *x, int i, int32_t v)
-{
-  (*x)[i] = v;
 }
 
 /* true if any elements of a vector compare result is non-zero.  */
@@ -208,11 +87,6 @@ v_call2_f32 (float (*f) (float, float), float32x4_t x1, float32x4_t x2,
 			p[3] ? f (x1[3], x2[3]) : y[3] };
 }
 
-static inline int
-v_lanes64 (void)
-{
-  return 2;
-}
 static inline float64x2_t
 v_f64 (double x)
 {
@@ -228,16 +102,7 @@ v_s64 (int64_t x)
 {
   return (int64x2_t) V2 (x);
 }
-static inline double
-v_get_f64 (float64x2_t x, int i)
-{
-  return x[i];
-}
-static inline void
-v_set_f64 (float64x2_t *x, int i, double v)
-{
-  (*x)[i] = v;
-}
+
 /* true if any elements of a vector compare result is non-zero.  */
 static inline int
 v_any_u64 (uint64x2_t x)
