aa5b3a0edfe9cd050c94881570ef9e7deb3db43e
Damian Heaton <Damian.Heaton@arm.com>
2023-06-30 10:30:47 +0000

    pl/math: Cleanup Neon f64 inverse trig functions
    Cleaned up the following Neon routines for consistency with other Neon
    routines, using a struct for constant values and using ACLE intrinsics
    rather than operators on vector types:
    - `asin`
    - `acos`
    - `atan`
    - `atan2`
    
    Also removed all vector supporting code from `atan_common.h`, as this is
    no longer used by the vector implementations.

--- pl/math/v_atan2_3u.c
+++ pl/math/v_atan2_3u.c
@@ -8,16 +8,35 @@
 #include "v_math.h"
 #include "pl_sig.h"
 #include "pl_test.h"
+#include "estrin.h"
 
-#include "atan_common.h"
+static const struct data
+{
+  float64x2_t pi_over_2;
+  float64x2_t poly[20];
+} data = {
+  /* Coefficients of polynomial P such that atan(x)~x+x*P(x^2) on
+     the interval [2**-1022, 1.0].  */
+  .poly = { V2 (-0x1.5555555555555p-2),	 V2 (0x1.99999999996c1p-3),
+	    V2 (-0x1.2492492478f88p-3),	 V2 (0x1.c71c71bc3951cp-4),
+	    V2 (-0x1.745d160a7e368p-4),	 V2 (0x1.3b139b6a88ba1p-4),
+	    V2 (-0x1.11100ee084227p-4),	 V2 (0x1.e1d0f9696f63bp-5),
+	    V2 (-0x1.aebfe7b418581p-5),	 V2 (0x1.842dbe9b0d916p-5),
+	    V2 (-0x1.5d30140ae5e99p-5),	 V2 (0x1.338e31eb2fbbcp-5),
+	    V2 (-0x1.00e6eece7de8p-5),	 V2 (0x1.860897b29e5efp-6),
+	    V2 (-0x1.0051381722a59p-6),	 V2 (0x1.14e9dc19a4a4ep-7),
+	    V2 (-0x1.d0062b42fe3bfp-9),	 V2 (0x1.17739e210171ap-10),
+	    V2 (-0x1.ab24da7be7402p-13), V2 (0x1.358851160a528p-16), },
+  .pi_over_2 = V2 (0x1.921fb54442d18p+0),
+};
 
-#define PiOver2 v_f64 (0x1.921fb54442d18p+0)
 #define SignMask v_u64 (0x8000000000000000)
 
+#define P(i) data_ptr->poly[i]
+
 /* Special cases i.e. 0, infinity, NaN (fall back to scalar calls).  */
-VPCS_ATTR
-NOINLINE static float64x2_t
-specialcase (float64x2_t y, float64x2_t x, float64x2_t ret, uint64x2_t cmp)
+static float64x2_t VPCS_ATTR NOINLINE
+special_case (float64x2_t y, float64x2_t x, float64x2_t ret, uint64x2_t cmp)
 {
   return v_call2_f64 (atan2, y, x, ret, cmp);
 }
@@ -26,51 +45,68 @@ specialcase (float64x2_t y, float64x2_t x, float64x2_t ret, uint64x2_t cmp)
 static inline uint64x2_t
 zeroinfnan (uint64x2_t i)
 {
-  return (2 * i - 1) >= v_u64 (2 * asuint64 (INFINITY) - 1);
+  /* (2 * i - 1) >= (2 * asuint64 (INFINITY) - 1).  */
+  return vcgeq_u64 (vsubq_u64 (vaddq_u64 (i, i), v_u64 (1)),
+		    v_u64 (2 * asuint64 (INFINITY) - 1));
 }
 
 /* Fast implementation of vector atan2.
    Maximum observed error is 2.8 ulps:
-   v_atan2(0x1.9651a429a859ap+5, 0x1.953075f4ee26p+5)
+   _ZGVnN2vv_atan2 (0x1.9651a429a859ap+5, 0x1.953075f4ee26p+5)
 	got 0x1.92d628ab678ccp-1
        want 0x1.92d628ab678cfp-1.  */
-VPCS_ATTR
-float64x2_t V_NAME_D2 (atan2) (float64x2_t y, float64x2_t x)
+float64x2_t VPCS_ATTR V_NAME_D2 (atan2) (float64x2_t y, float64x2_t x)
 {
+  const struct data *data_ptr = ptr_barrier (&data);
+
   uint64x2_t ix = vreinterpretq_u64_f64 (x);
   uint64x2_t iy = vreinterpretq_u64_f64 (y);
 
-  uint64x2_t special_cases = zeroinfnan (ix) | zeroinfnan (iy);
+  uint64x2_t special_cases = vorrq_u64 (zeroinfnan (ix), zeroinfnan (iy));
 
-  uint64x2_t sign_x = ix & SignMask;
-  uint64x2_t sign_y = iy & SignMask;
-  uint64x2_t sign_xy = sign_x ^ sign_y;
+  uint64x2_t sign_x = vandq_u64 (ix, SignMask);
+  uint64x2_t sign_y = vandq_u64 (iy, SignMask);
+  uint64x2_t sign_xy = veorq_u64 (sign_x, sign_y);
 
   float64x2_t ax = vabsq_f64 (x);
   float64x2_t ay = vabsq_f64 (y);
 
-  uint64x2_t pred_xlt0 = x < 0.0;
-  uint64x2_t pred_aygtax = ay > ax;
+  uint64x2_t pred_xlt0 = vcltzq_f64 (x);
+  uint64x2_t pred_aygtax = vcgtq_f64 (ay, ax);
 
   /* Set up z for call to atan.  */
-  float64x2_t n = vbslq_f64 (pred_aygtax, -ax, ay);
+  float64x2_t n = vbslq_f64 (pred_aygtax, vnegq_f64 (ax), ay);
   float64x2_t d = vbslq_f64 (pred_aygtax, ay, ax);
   float64x2_t z = vdivq_f64 (n, d);
 
   /* Work out the correct shift.  */
-  float64x2_t shift = vbslq_f64 (pred_xlt0, v_f64 (-2.0), v_f64 (0.0));
-  shift = vbslq_f64 (pred_aygtax, shift + 1.0, shift);
-  shift *= PiOver2;
+  float64x2_t shift = vreinterpretq_f64_u64 (
+      vandq_u64 (pred_xlt0, vreinterpretq_u64_f64 (v_f64 (-2.0))));
+  shift = vbslq_f64 (pred_aygtax, vaddq_f64 (shift, v_f64 (1.0)), shift);
+  shift = vmulq_f64 (shift, data_ptr->pi_over_2);
 
-  float64x2_t ret = eval_poly (z, z, shift);
+  /* Calculate the polynomial approximation.
+     Use split Estrin scheme for P(z^2) with deg(P)=19. Use split instead of
+     full scheme to avoid underflow in x^16.
+     The order 19 polynomial P approximates
+     (atan(sqrt(x))-sqrt(x))/x^(3/2).  */
+  float64x2_t z2 = vmulq_f64 (z, z);
+  float64x2_t x2 = vmulq_f64 (z2, z2);
+  float64x2_t x4 = vmulq_f64 (x2, x2);
+  float64x2_t x8 = vmulq_f64 (x4, x4);
+  float64x2_t ret = vfmaq_f64 (ESTRIN_7 (z2, x2, x4, P),
+			       ESTRIN_11_ (z2, x2, x4, x8, P, 8), x8);
+
+  /* Finalize. y = shift + z + z^3 * P(z^2).  */
+  ret = vfmaq_f64 (z, ret, vmulq_f64 (z2, z));
+  ret = vaddq_f64 (ret, shift);
 
   /* Account for the sign of x and y.  */
-  ret = vreinterpretq_f64_u64 (vreinterpretq_u64_f64 (ret) ^ sign_xy);
+  ret = vreinterpretq_f64_u64 (
+      veorq_u64 (vreinterpretq_u64_f64 (ret), sign_xy));
 
   if (unlikely (v_any_u64 (special_cases)))
-    {
-      return specialcase (y, x, ret, special_cases);
-    }
+    return special_case (y, x, ret, special_cases);
 
   return ret;
 }
--- pl/math/v_atan_2u5.c
+++ pl/math/v_atan_2u5.c
@@ -8,31 +8,53 @@
 #include "v_math.h"
 #include "pl_sig.h"
 #include "pl_test.h"
+#include "estrin.h"
 
-#include "atan_common.h"
+static const struct data
+{
+  float64x2_t pi_over_2;
+  float64x2_t poly[20];
+} data = {
+  /* Coefficients of polynomial P such that atan(x)~x+x*P(x^2) on
+	      [2**-1022, 1.0].  */
+  .poly = { V2 (-0x1.5555555555555p-2),	 V2 (0x1.99999999996c1p-3),
+	    V2 (-0x1.2492492478f88p-3),	 V2 (0x1.c71c71bc3951cp-4),
+	    V2 (-0x1.745d160a7e368p-4),	 V2 (0x1.3b139b6a88ba1p-4),
+	    V2 (-0x1.11100ee084227p-4),	 V2 (0x1.e1d0f9696f63bp-5),
+	    V2 (-0x1.aebfe7b418581p-5),	 V2 (0x1.842dbe9b0d916p-5),
+	    V2 (-0x1.5d30140ae5e99p-5),	 V2 (0x1.338e31eb2fbbcp-5),
+	    V2 (-0x1.00e6eece7de8p-5),	 V2 (0x1.860897b29e5efp-6),
+	    V2 (-0x1.0051381722a59p-6),	 V2 (0x1.14e9dc19a4a4ep-7),
+	    V2 (-0x1.d0062b42fe3bfp-9),	 V2 (0x1.17739e210171ap-10),
+	    V2 (-0x1.ab24da7be7402p-13), V2 (0x1.358851160a528p-16), },
+  .pi_over_2 = V2 (0x1.921fb54442d18p+0),
+};
 
-#define PiOver2 v_f64 (0x1.921fb54442d18p+0)
-#define AbsMask v_u64 (0x7fffffffffffffff)
-#define TinyBound 0x3e1 /* top12(asuint64(0x1p-30)).  */
-#define BigBound 0x434	/* top12(asuint64(0x1p53)).  */
+#define SignMask v_u64 (0x8000000000000000)
+#define TinyBound 0x3e10000000000000 /* asuint64(0x1p-30).  */
+#define BigBound 0x4340000000000000  /* asuint64(0x1p53).  */
+
+#define P(i) d->poly[i]
 
 /* Fast implementation of vector atan.
    Based on atan(x) ~ shift + z + z^3 * P(z^2) with reduction to [0,1] using
    z=1/x and shift = pi/2. Maximum observed error is 2.27 ulps:
-   __v_atan(0x1.0005af27c23e9p+0) got 0x1.9225645bdd7c1p-1
-				 want 0x1.9225645bdd7c3p-1.  */
-VPCS_ATTR
-float64x2_t V_NAME_D1 (atan) (float64x2_t x)
+   _ZGVnN2v_atan (0x1.0005af27c23e9p+0) got 0x1.9225645bdd7c1p-1
+				       want 0x1.9225645bdd7c3p-1.  */
+float64x2_t VPCS_ATTR V_NAME_D1 (atan) (float64x2_t x)
 {
+  const struct data *d = ptr_barrier (&data);
+
   /* Small cases, infs and nans are supported by our approximation technique,
      but do not set fenv flags correctly. Only trigger special case if we need
      fenv.  */
   uint64x2_t ix = vreinterpretq_u64_f64 (x);
-  uint64x2_t sign = ix & ~AbsMask;
+  uint64x2_t sign = vandq_u64 (ix, SignMask);
 
 #if WANT_SIMD_EXCEPT
-  uint64x2_t ia12 = (ix >> 52) & 0x7ff;
-  uint64x2_t special = ia12 - TinyBound > BigBound - TinyBound;
+  uint64x2_t ia12 = vandq_u64 (ix, v_u64 (0x7ff0000000000000));
+  uint64x2_t special = vcgtq_u64 (vsubq_u64 (ia12, v_u64 (TinyBound)),
+				  v_u64 (BigBound - TinyBound));
   /* If any lane is special, fall back to the scalar routine for all lanes.  */
   if (unlikely (v_any_u64 (special)))
     return v_call_f64 (atan, x, v_f64 (0), v_u64 (-1));
@@ -44,17 +66,31 @@ float64x2_t V_NAME_D1 (atan) (float64x2_t x)
      Hence, use z=-1/a if x>=1, otherwise z=a.  */
   uint64x2_t red = vcagtq_f64 (x, v_f64 (1.0));
   /* Avoid dependency in abs(x) in division (and comparison).  */
-  float64x2_t z = vbslq_f64 (red, vdivq_f64 (v_f64 (-1.0), x), x);
-  float64x2_t shift = vbslq_f64 (red, PiOver2, v_f64 (0.0));
+  float64x2_t z = vbslq_f64 (red, vdivq_f64 (v_f64 (1.0), x), x);
+  float64x2_t shift = vreinterpretq_f64_u64 (
+      vandq_u64 (red, vreinterpretq_u64_f64 (d->pi_over_2)));
   /* Use absolute value only when needed (odd powers of z).  */
-  float64x2_t az = vabsq_f64 (z);
-  az = vbslq_f64 (red, -az, az);
+  float64x2_t az = vbslq_f64 (
+      SignMask, vreinterpretq_f64_u64 (vandq_u64 (SignMask, red)), z);
 
-  /* Calculate the polynomial approximation.  */
-  float64x2_t y = eval_poly (z, az, shift);
+  /* Calculate the polynomial approximation.
+     Use split Estrin scheme for P(z^2) with deg(P)=19. Use split instead of
+     full scheme to avoid underflow in x^16.
+     The order 19 polynomial P approximates
+     (atan(sqrt(x))-sqrt(x))/x^(3/2).  */
+  float64x2_t z2 = vmulq_f64 (z, z);
+  float64x2_t x2 = vmulq_f64 (z2, z2);
+  float64x2_t x4 = vmulq_f64 (x2, x2);
+  float64x2_t x8 = vmulq_f64 (x4, x4);
+  float64x2_t y = vfmaq_f64 (ESTRIN_7 (z2, x2, x4, P),
+			     ESTRIN_11_ (z2, x2, x4, x8, P, 8), x8);
+
+  /* Finalize. y = shift + z + z^3 * P(z^2).  */
+  y = vfmaq_f64 (az, y, vmulq_f64 (z2, az));
+  y = vaddq_f64 (y, shift);
 
   /* y = atan(x) if x>0, -atan(-x) otherwise.  */
-  y = vreinterpretq_f64_u64 (vreinterpretq_u64_f64 (y) ^ sign);
+  y = vreinterpretq_f64_u64 (veorq_u64 (vreinterpretq_u64_f64 (y), sign));
   return y;
 }
 
