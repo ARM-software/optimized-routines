3350e22734e572a9271d74ffb0f9873bccd562a9
Joana Cruz <Joana.Cruz@arm.com>
2023-10-31 16:16:58 +0000

    pl/math: Implement SVE atanhf
    Adds an SVE implementation of single-precision atanhf, based
    on the existing Neon implementation.
    
    ULP Max Error:
    _ZGVsMxv_atanhf 2.28 ULP

--- pl/math/include/mathlib.h
+++ pl/math/include/mathlib.h
@@ -145,6 +145,7 @@ svfloat32_t _ZGVsMxv_asinhf (svfloat32_t, svbool_t);
 svfloat64_t _ZGVsMxv_asinh (svfloat64_t, svbool_t);
 svfloat32_t _ZGVsMxv_asinf (svfloat32_t, svbool_t);
 svfloat64_t _ZGVsMxv_asin (svfloat64_t, svbool_t);
+svfloat32_t _ZGVsMxv_atanhf (svfloat32_t, svbool_t);
 svfloat32_t _ZGVsMxvv_atan2f (svfloat32_t, svfloat32_t, svbool_t);
 svfloat32_t _ZGVsMxv_atanf (svfloat32_t, svbool_t);
 svfloat64_t _ZGVsMxv_atan (svfloat64_t, svbool_t);
