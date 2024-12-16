a386525a014d7ab8c5d689244ff19ce4e341d991
Yat Long Poon <YatLong.Poon@arm.com>
2024-12-16 10:42:10 +0000

    math/aarch64/sve: Improve codegen for log1pf and users
    Improvements done:
    - Use lanewise MLA
    - Reduce number of MOVPRFX/MOVs
    - Use inline helper for log1pf

--- math/aarch64/sve/acoshf.c
+++ math/aarch64/sve/acoshf.c
@@ -14,17 +14,19 @@
 #include "sv_log1pf_inline.h"
 
 static svfloat32_t NOINLINE
-special_case (svfloat32_t x, svfloat32_t y, svbool_t special)
+special_case (svfloat32_t xm1, svfloat32_t tmp, svbool_t special)
 {
+  svfloat32_t x = svadd_x (svptrue_b32 (), xm1, 1.0f);
+  svfloat32_t y = sv_log1pf_inline (tmp, svptrue_b32 ());
   return sv_call_f32 (acoshf, x, y, special);
 }
 
 /* Single-precision SVE acosh(x) routine. Implements the same algorithm as
    vector acoshf and log1p.
 
-   Maximum error is 2.78 ULPs:
-   SV_NAME_F1 (acosh) (0x1.01e996p+0) got 0x1.f45b42p-4
-				     want 0x1.f45b3cp-4.  */
+   Maximum error is 2.47 ULPs:
+   SV_NAME_F1 (acosh) (0x1.01ca76p+0) got 0x1.e435a6p-4
+				     want 0x1.e435a2p-4.  */
 svfloat32_t SV_NAME_F1 (acosh) (svfloat32_t x, const svbool_t pg)
 {
   svuint32_t ix = svreinterpret_u32 (x);
@@ -32,15 +34,15 @@ svfloat32_t SV_NAME_F1 (acosh) (svfloat32_t x, const svbool_t pg)
 
   svfloat32_t xm1 = svsub_x (pg, x, 1.0f);
   svfloat32_t u = svmul_x (pg, xm1, svadd_x (pg, x, 1.0f));
-  svfloat32_t y = sv_log1pf_inline (svadd_x (pg, xm1, svsqrt_x (pg, u)), pg);
+  svfloat32_t tmp = svadd_x (pg, xm1, svsqrt_x (pg, u));
 
   if (unlikely (svptest_any (pg, special)))
-    return special_case (x, y, special);
-  return y;
+    return special_case (xm1, tmp, special);
+  return sv_log1pf_inline (tmp, pg);
 }
 
 TEST_SIG (SV, F, 1, acosh, 1.0, 10.0)
-TEST_ULP (SV_NAME_F1 (acosh), 2.29)
+TEST_ULP (SV_NAME_F1 (acosh), 1.97)
 TEST_DISABLE_FENV (SV_NAME_F1 (acosh))
 TEST_INTERVAL (SV_NAME_F1 (acosh), 0, 1, 500)
 TEST_INTERVAL (SV_NAME_F1 (acosh), 1, 0x1p64, 100000)
--- math/aarch64/sve/atanhf.c
+++ math/aarch64/sve/atanhf.c
@@ -6,7 +6,6 @@
  */
 
 #include "sv_math.h"
-#include "mathlib.h"
 #include "test_sig.h"
 #include "test_defs.h"
 
@@ -16,15 +15,18 @@
 #define Half (0x3f000000)
 
 static svfloat32_t NOINLINE
-special_case (svfloat32_t x, svfloat32_t y, svbool_t special)
+special_case (svuint32_t iax, svuint32_t sign, svfloat32_t halfsign,
+	      svfloat32_t y, svbool_t special)
 {
+  svfloat32_t x = svreinterpret_f32 (sveor_x (svptrue_b32 (), iax, sign));
+  y = svmul_x (svptrue_b32 (), halfsign, y);
   return sv_call_f32 (atanhf, x, y, special);
 }
 
 /* Approximation for vector single-precision atanh(x) using modified log1p.
-   The maximum error is 2.28 ULP:
-   _ZGVsMxv_atanhf(0x1.ff1194p-5) got 0x1.ffbbbcp-5
-				 want 0x1.ffbbb6p-5.  */
+   The maximum error is 1.99 ULP:
+   _ZGVsMxv_atanhf(0x1.f1583p-5) got 0x1.f1f4fap-5
+				want 0x1.f1f4f6p-5.  */
 svfloat32_t SV_NAME_F1 (atanh) (svfloat32_t x, const svbool_t pg)
 {
   svfloat32_t ax = svabs_x (pg, x);
@@ -41,13 +43,13 @@ svfloat32_t SV_NAME_F1 (atanh) (svfloat32_t x, const svbool_t pg)
   y = sv_log1pf_inline (y, pg);
 
   if (unlikely (svptest_any (pg, special)))
-    return special_case (x, svmul_x (pg, halfsign, y), special);
+    return special_case (iax, sign, halfsign, y, special);
 
   return svmul_x (pg, halfsign, y);
 }
 
 TEST_SIG (SV, F, 1, atanh, -1.0, 1.0)
-TEST_ULP (SV_NAME_F1 (atanh), 2.59)
+TEST_ULP (SV_NAME_F1 (atanh), 1.50)
 TEST_DISABLE_FENV (SV_NAME_F1 (atanh))
 TEST_SYM_INTERVAL (SV_NAME_F1 (atanh), 0, 0x1p-12, 1000)
 TEST_SYM_INTERVAL (SV_NAME_F1 (atanh), 0x1p-12, 1, 20000)
