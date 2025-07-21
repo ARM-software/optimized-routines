cedf809e27d3fe878a650c1aed29576fb50d1b59
Simon Tatham <simon.tatham@arm.com>
2025-07-21 08:58:41 +0100

    fp: replace stub test-ui2f.c with a real one.
    Somehow I failed to notice that I'd made this test file just a
    framework, without putting any actual test cases in the tests[] array,
    so it wasn't doing anything! Not only that, but it contained a call to
    the wrong function: if there had been any test cases, it would have
    run them using the wrong function, testing arm_fp_i2f rather than
    arm_fp_ui2f.
    
    Both fixed. Fortunately, the actual implementation of arm_fp_ui2f
    seems to have been correct: it passes all the new tests already. Only
    the test program was erroneous.

--- fp/test/test-ui2f.c
+++ fp/test/test-ui2f.c
@@ -17,6 +17,25 @@ struct test
 };
 
 static const struct test tests[] = {
+  { 0x00000000, 0x00000000 },
+  { 0x00000001, 0x3f800000 },
+  { 0x08000000, 0x4d000000 },
+  { 0x08000004, 0x4d000000 },
+  { 0x08000008, 0x4d000000 },
+  { 0x0800000c, 0x4d000001 },
+  { 0x08000010, 0x4d000001 },
+  { 0x08000014, 0x4d000001 },
+  { 0x08000018, 0x4d000002 },
+  { 0x0800001c, 0x4d000002 },
+  { 0xfffffe00, 0x4f7ffffe },
+  { 0xfffffe7f, 0x4f7ffffe },
+  { 0xfffffe80, 0x4f7ffffe },
+  { 0xfffffe81, 0x4f7fffff },
+  { 0xffffff00, 0x4f7fffff },
+  { 0xffffff7f, 0x4f7fffff },
+  { 0xffffff80, 0x4f800000 },
+  { 0xffffff81, 0x4f800000 },
+  { 0xffffffff, 0x4f800000 },
 };
 
 float
@@ -51,13 +70,13 @@ main (void)
       float out = (float)t->in;
       uint32_t outbits = unmake_float(out);
 #else
-      extern uint32_t arm_fp_i2f(uint32_t);
-      uint32_t outbits = arm_fp_i2f(t->in);
+      extern uint32_t arm_fp_ui2f(uint32_t);
+      uint32_t outbits = arm_fp_ui2f(t->in);
 #endif
 
       if (outbits != t->out)
 	{
-	  printf ("FAIL: i2f(%08" PRIx32 ") -> %08" PRIx32
+	  printf ("FAIL: ui2f(%08" PRIx32 ") -> %08" PRIx32
 		  ", expected %08" PRIx32 "\n", t->in, outbits, t->out);
 	  failed = true;
 	}
