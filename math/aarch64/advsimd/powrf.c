5b06a16101f60f2a8e09dd46a4faef351595cc65
Pierre Blanchard <pierre.blanchard@arm.com>
2025-11-04 13:30:20 +0000

    math/aarch64/advsimd: Optimize pow(r)f.
    Finally account for simplifications allowed by powr's definition.
    
    Provide helpers with and without sign_bias so that
    - powf can use sign bias in low path and no sign bias in fast path
    - powrf never uses sign bias
    
    Moved some helpers into top level source and only keep core computation
    in inline helper files.
    
    Question mark on supporting negative x outside of fast path for powrf.
    It is probably not that easy to do it efficiently and does not cost
    much to leave it in slow path.

--- math/aarch64/advsimd/powrf.c
+++ math/aarch64/advsimd/powrf.c
@@ -6,8 +6,64 @@
  */
 
 #include "v_math.h"
+#include "v_powrf_inline.h"
 #include "test_defs.h"
-#include "v_powf_inline.h"
+
+/* A scalar subroutine used to fix main power special cases. Similar to the
+   preamble of scalar powf except that we do not update ix and sign_bias. This
+   is done in the preamble of the SVE powf.  */
+static inline float
+powrf_specialcase (float x, float y)
+{
+  uint32_t ix = asuint (x);
+  uint32_t iy = asuint (y);
+  /* Either x or y is 0 or inf or nan.  */
+  if (unlikely (zeroinfnan (iy)))
+    {
+      if (2 * iy == 0)
+	return issignalingf_inline (x) ? x + y : 1.0f;
+      if (ix == 0x3f800000)
+	return issignalingf_inline (y) ? x + y : 1.0f;
+      if (2 * ix > 2u * 0x7f800000 || 2 * iy > 2u * 0x7f800000)
+	return x + y;
+      if ((2 * ix < 2 * 0x3f800000) == !(iy & 0x80000000))
+	return 0.0f; /* |x|<1 && y==inf or |x|>1 && y==-inf.  */
+      return y * y;
+    }
+  if (unlikely (zeroinfnan (ix)))
+    {
+      float x2 = x * x;
+      return iy & 0x80000000 ? 1 / x2 : x2;
+    }
+  return x;
+}
+
+/* Special case function wrapper.  */
+static float32x4_t VPCS_ATTR NOINLINE
+special_case (float32x4_t x, float32x4_t y, float32x4_t ret, uint32x4_t cmp)
+{
+  return v_call2_f32 (powrf_specialcase, x, y, ret, cmp);
+}
+
+/* Power implementation for x containing negative or subnormal lanes.  */
+static inline float32x4_t
+v_powrf_x_is_neg_or_sub (float32x4_t x, float32x4_t y, const struct data *d)
+{
+  uint32x4_t xsmall = vcaltq_f32 (x, v_f32 (0x1p-126f));
+
+  /* Normalize subnormals.  */
+  float32x4_t a = vabsq_f32 (x);
+  uint32x4_t ia_norm = vreinterpretq_u32_f32 (vmulq_f32 (a, d->norm));
+  ia_norm = vsubq_u32 (ia_norm, d->subnormal_bias);
+  a = vbslq_f32 (xsmall, vreinterpretq_f32_u32 (ia_norm), a);
+
+  /* Evaluate exp (y * log(x)) using |x| and sign bias correction.  */
+  float32x4_t ret = v_powrf_core (a, y, d);
+
+  /* Cases of finite y and finite negative x.  */
+  uint32x4_t xisneg = vcltzq_f32 (x);
+  return vbslq_f32 (xisneg, d->nan, ret);
+}
 
 /* Implementation of AdvSIMD powrf.
      powr(x,y) := exp(y * log (x))
@@ -20,11 +76,29 @@
     want 0x1.fff862p+127.  */
 float32x4_t VPCS_ATTR NOINLINE V_NAME_F2 (powr) (float32x4_t x, float32x4_t y)
 {
-  /* Current powf core treats x negative and y integer differently,
-     for the sake of clarity we fix that in powr using extra operations.
-     Following patches will rely on a simplified core instead.  */
-  uint32x4_t pos = vcgezq_f32 (x);
-  return vbslq_f32 (pos, v_powf_inline (x, y), v_f32 (__builtin_nanf ("")));
+  const struct data *d = ptr_barrier (&data);
+
+  /* Special cases of x or y: zero, inf and nan.  */
+  uint32x4_t ix = vreinterpretq_u32_f32 (x);
+  uint32x4_t iy = vreinterpretq_u32_f32 (y);
+  uint32x4_t xspecial = v_zeroinfnan (d, ix);
+  uint32x4_t yspecial = v_zeroinfnan (d, iy);
+  uint32x4_t cmp = vorrq_u32 (xspecial, yspecial);
+
+  /* Evaluate pow(x, y) for x containing negative or subnormal lanes.  */
+  uint32x4_t x_is_neg_or_sub = vcltq_f32 (x, v_f32 (0x1p-126f));
+  if (unlikely (v_any_u32 (x_is_neg_or_sub)))
+    {
+      float32x4_t ret = v_powrf_x_is_neg_or_sub (x, y, d);
+      if (unlikely (v_any_u32 (cmp)))
+	return special_case (x, y, ret, cmp);
+      return ret;
+    }
+
+  /* Else evaluate pow(x, y) for normal and positive x only.  */
+  if (unlikely (v_any_u32 (cmp)))
+    return special_case (x, y, v_powrf_core (x, y, d), cmp);
+  return v_powrf_core (x, y, d);
 }
 
 HALF_WIDTH_ALIAS_F2 (powr)
