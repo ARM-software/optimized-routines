281cb3ab756c3f824fc85a832d282d6776fc783a
Cosmina Dunca <Cosmina.Dunca@arm.com>
2026-01-27 17:40:31 +0000

    math/aarch64/advsimd: Vectorize special cases for atanh
    Since atanh is defined on [-1, 1], the special cases are any other
    number that is not in this interval. In cases where x is not +1 or -1,
    the function returns NaN, which makes it easy to return a select (vbslq)
    at the end of the special_case function.
    
    However, there is one more special case for +1/-1 where we'll need to
    return +/- infinity and this requires an extra select itself too, but we
    do a bit select to add the sign of x and keep the rest as infinity.
    
    We'll do a select in the special case which returns either NaN or the
    result or the number it is meant to return.

--- math/aarch64/advsimd/atanh.c
+++ math/aarch64/advsimd/atanh.c
@@ -1,7 +1,7 @@
 /*
  * Double-precision vector atanh(x) function.
  *
- * Copyright (c) 2022-2025, Arm Limited.
+ * Copyright (c) 2022-2026, Arm Limited.
  * SPDX-License-Identifier: MIT OR Apache-2.0 WITH LLVM-exception
  */
 
@@ -15,23 +15,30 @@
 const static struct data
 {
   struct v_log1p_data log1p_consts;
-  uint64x2_t one;
+  float64x2_t one;
   uint64x2_t sign_mask;
 } data = { .log1p_consts = V_LOG1P_CONSTANTS_TABLE,
-	   .one = V2 (0x3ff0000000000000),
+	   .one = V2 (1.0),
 	   .sign_mask = V2 (0x8000000000000000) };
 
 static float64x2_t VPCS_ATTR NOINLINE
-special_case (float64x2_t x, float64x2_t halfsign, float64x2_t y,
+special_case (float64x2_t ax, float64x2_t halfsign, float64x2_t y,
 	      uint64x2_t special, const struct data *d)
 {
   y = log1p_inline (y, &d->log1p_consts);
-  return v_call_f64 (atanh, vbslq_f64 (d->sign_mask, halfsign, x),
-		     vmulq_f64 (halfsign, y), special);
+  /* Copy the sign bit from the input to inf.  */
+  uint64x2_t is_one = vceqq_f64 (ax, d->one);
+  float64x2_t signed_inf
+      = vbslq_f64 (d->sign_mask, halfsign, v_f64 (INFINITY));
+  /* Here we check for all the rest of the cases where |x| > 1 will return a
+     NaN, including if x = NaN.  */
+  float64x2_t res = vbslq_f64 (special, v_f64 (NAN), vmulq_f64 (halfsign, y));
+
+  return vbslq_f64 (is_one, signed_inf, res);
 }
 
 /* Approximation for vector double-precision atanh(x) using modified log1p.
-   The greatest observed error is 3.31 ULP:
+   The greatest observed error is 2.81 + 0.5 ULP:
    _ZGVnN2v_atanh(0x1.ffae6288b601p-6) got 0x1.ffd8ff31b5019p-6
 				      want 0x1.ffd8ff31b501cp-6.  */
 VPCS_ATTR float64x2_t V_NAME_D1 (atanh) (float64x2_t x)
@@ -40,12 +47,11 @@ VPCS_ATTR float64x2_t V_NAME_D1 (atanh) (float64x2_t x)
 
   float64x2_t halfsign = vbslq_f64 (d->sign_mask, x, v_f64 (0.5));
   float64x2_t ax = vabsq_f64 (x);
-  uint64x2_t ia = vreinterpretq_u64_f64 (ax);
-  uint64x2_t special = vcgeq_u64 (ia, d->one);
+  uint64x2_t special = vcageq_f64 (x, d->one);
 
   float64x2_t y;
   y = vaddq_f64 (ax, ax);
-  y = vdivq_f64 (y, vsubq_f64 (vreinterpretq_f64_u64 (d->one), ax));
+  y = vdivq_f64 (y, vsubq_f64 (d->one, ax));
 
   if (unlikely (v_any_u64 (special)))
     return special_case (ax, halfsign, y, special, d);
@@ -55,11 +61,7 @@ VPCS_ATTR float64x2_t V_NAME_D1 (atanh) (float64x2_t x)
 }
 
 TEST_SIG (V, D, 1, atanh, -1.0, 1.0)
-TEST_ULP (V_NAME_D1 (atanh), 3.32)
+TEST_ULP (V_NAME_D1 (atanh), 2.81)
 TEST_SYM_INTERVAL (V_NAME_D1 (atanh), 0, 0x1p-23, 10000)
 TEST_SYM_INTERVAL (V_NAME_D1 (atanh), 0x1p-23, 1, 90000)
 TEST_SYM_INTERVAL (V_NAME_D1 (atanh), 1, inf, 100)
-/* atanh is asymptotic at 1, which is the default control value - have to set
-   -c 0 specially to ensure fp exceptions are triggered correctly (choice of
-   control lane is irrelevant if fp exceptions are disabled).  */
-TEST_CONTROL_VALUE (V_NAME_D1 (atanh), 0)
