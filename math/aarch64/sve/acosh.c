8b665af848a9e31d4cc4a46cf7f4389e930b2da7
Cosmina Dunca <Cosmina.Dunca@arm.com>
2026-02-04 12:07:48 +0000

    math/aarch64/sve: Vectorize special cases for acosh
    For acosh there are plenty of special cases to consider, such as
    infinity, undefined values (everything < 1) and 1. However, some of
    these are already handled by other calculations in this algorithm
    such as the case of x = 1 that will be handled by log1p.
    
    Infinity will also be calculated as ln(inf) + ln(2) and we know that any
    calculation that includes infinity returns infinity.
    
    However, there are the cases of x < 1 that need to be checked, as well
    as NaN itself and all of these possibilities will return NaN.
    
    The method used to calculate acosh here is just the same as the AdvSIMD
    acosh function. When x gets very big, '-1' in the acosh formula is
    negligible and it can be overlooked, which will give us acosh(x) = ln(x)
    + ln(2).

--- math/aarch64/sve/acosh.c
+++ math/aarch64/sve/acosh.c
@@ -1,6 +1,6 @@
 /*
  * Double-precision SVE acosh(x) function.
- * Copyright (c) 2024-2025, Arm Limited.
+ * Copyright (c) 2024-2026, Arm Limited.
  * SPDX-License-Identifier: MIT OR Apache-2.0 WITH LLVM-exception
  */
 
@@ -11,13 +11,33 @@
 #define WANT_SV_LOG1P_K0_SHORTCUT 1
 #include "sv_log1p_inline.h"
 
-#define One (0x3ff0000000000000)
-#define Thres (0x1ff0000000000000) /* asuint64 (0x1p511) - One.  */
+#define One (0x3ff0000000000000ULL)
+#define Thres (0x1ff0000000000000ULL)
 
-static svfloat64_t NOINLINE
-special_case (svfloat64_t x, svfloat64_t y, svbool_t special)
+const static struct data
 {
-  return sv_call_f64 (acosh, x, y, special);
+  double ln2;
+} data = { .ln2 = 0x1.62e42fefa39efp-1 };
+
+/* Acosh is define on [1, inf). Its formula can be re-written knowing that 1
+   becomes negligible when x is a very large number. So for special numbers,
+   where x >= 2^511, acosh ~= ln(2x). But, ln(2x) = ln(2) + ln(x) and below we
+   calculate ln(x) and then add ln(2) to the result.
+
+   Right before returning we check if x is infinity or if x is lower than 1,
+   in which case we return infinity or NaN.  */
+static svfloat64_t NOINLINE
+special_case (svfloat64_t x, svfloat64_t xm1, svfloat64_t y, svbool_t special,
+	      svbool_t pg, const struct data *d)
+{
+  svfloat64_t inf = sv_f64 (INFINITY);
+  svfloat64_t nan = sv_f64 (NAN);
+  svfloat64_t log = sv_log1p_inline (svsel (special, xm1, y), pg);
+  svfloat64_t result = svadd_m (special, log, sv_f64 (d->ln2));
+  svbool_t is_inf_nan = svorr_b_z (special, svcmpge (special, x, inf),
+				   svcmplt (special, x, sv_f64 (1.0)));
+  svfloat64_t inf_or_nan = svsel (svcmpge (special, x, inf), inf, nan);
+  return svsel (is_inf_nan, inf_or_nan, result);
 }
 
 /* SVE approximation for double-precision acosh, based on log1p.
@@ -27,6 +47,8 @@ special_case (svfloat64_t x, svfloat64_t y, svbool_t special)
 					   want 0x1.ef0cee7c33ce4p-2.  */
 svfloat64_t SV_NAME_D1 (acosh) (svfloat64_t x, const svbool_t pg)
 {
+  const struct data *d = ptr_barrier (&data);
+
   /* (ix - One) >= (BigBound - One).  */
   svuint64_t ix = svreinterpret_u64 (x);
   svbool_t special = svcmpge (pg, svsub_x (pg, ix, One), Thres);
@@ -37,7 +59,7 @@ svfloat64_t SV_NAME_D1 (acosh) (svfloat64_t x, const svbool_t pg)
 
   /* Fall back to scalar routine for special lanes.  */
   if (unlikely (svptest_any (pg, special)))
-    return special_case (x, sv_log1p_inline (y, pg), special);
+    return special_case (x, xm1, y, special, pg, d);
   return sv_log1p_inline (y, pg);
 }
 
