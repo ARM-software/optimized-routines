68d894a17511d46d954d2736eb4ace82a0478d92
Thomas Daubney <Thomas.Daubney@arm.com>
2026-05-05 15:19:25 +0000

    math/aarch64/sve: Vectorise special case for atanhf
    Implement vectorised fallback for atanhf special cases.

--- math/aarch64/sve/atanhf.c
+++ math/aarch64/sve/atanhf.c
@@ -1,7 +1,7 @@
 /*
  * Single-precision vector atanh(x) function.
  *
- * Copyright (c) 2023-2025, Arm Limited.
+ * Copyright (c) 2023-2026, Arm Limited.
  * SPDX-License-Identifier: MIT OR Apache-2.0 WITH LLVM-exception
  */
 
@@ -11,16 +11,14 @@
 
 #include "sv_log1pf_inline.h"
 
-#define One (0x3f800000)
-#define Half (0x3f000000)
-
-static svfloat32_t NOINLINE
-special_case (svuint32_t iax, svuint32_t sign, svfloat32_t halfsign,
-	      svfloat32_t y, svbool_t special)
+static svfloat32_t inline special_case (svfloat32_t ax, svfloat32_t y,
+					svbool_t special, svfloat32_t halfsign,
+					const struct sv_log1pf_data *d)
 {
-  svfloat32_t x = svreinterpret_f32 (sveor_x (svptrue_b32 (), iax, sign));
-  y = svmul_x (svptrue_b32 (), halfsign, y);
-  return sv_call_f32 (atanhf, x, y, special);
+  svfloat32_t res = svsel (special, svreinterpret_f32 (sv_u32 (d->nan)), y);
+  res = svsel (svcmpeq (special, ax, sv_f32 (1.0)),
+	       svreinterpret_f32 (sv_u32 (d->inf)), res);
+  return svmul_x (svptrue_b32 (), res, halfsign);
 }
 
 /* Approximation for vector single-precision atanh(x) using modified log1p.
@@ -29,23 +27,26 @@ special_case (svuint32_t iax, svuint32_t sign, svfloat32_t halfsign,
 				want 0x1.f1f4f6p-5.  */
 svfloat32_t SV_NAME_F1 (atanh) (svfloat32_t x, const svbool_t pg)
 {
+  const struct sv_log1pf_data *d = ptr_barrier (&sv_log1pf_data);
+
   svfloat32_t ax = svabs_x (pg, x);
   svuint32_t iax = svreinterpret_u32 (ax);
-  svuint32_t sign = sveor_x (pg, svreinterpret_u32 (x), iax);
-  svfloat32_t halfsign = svreinterpret_f32 (svorr_x (pg, sign, Half));
-  svbool_t special = svcmpge (pg, iax, One);
+  svuint32_t sign = sveor_x (svptrue_b32 (), svreinterpret_u32 (x), iax);
+  svfloat32_t halfsign
+      = svreinterpret_f32 (svorr_x (svptrue_b32 (), sign, 0x3f000000));
+  svbool_t special = svcmpge (pg, iax, svreinterpret_u32 (sv_f32 (1)));
 
   /* Computation is performed based on the following sequence of equality:
-   * (1+x)/(1-x) = 1 + 2x/(1-x).  */
-  svfloat32_t y = svadd_x (pg, ax, ax);
-  y = svdiv_x (pg, y, svsub_x (pg, sv_f32 (1), ax));
+     (1+x)/(1-x) = 1 + 2x/(1-x).  */
+  svfloat32_t y = svadd_x (svptrue_b32 (), ax, ax);
+  y = svdiv_x (pg, y, svsubr_x (pg, ax, 1.0f));
   /* ln((1+x)/(1-x)) = ln(1+2x/(1-x)) = ln(1 + y).  */
   y = sv_log1pf_inline (y, pg);
 
   if (unlikely (svptest_any (pg, special)))
-    return special_case (iax, sign, halfsign, y, special);
+    return special_case (ax, y, special, halfsign, d);
 
-  return svmul_x (pg, halfsign, y);
+  return svmul_x (svptrue_b32 (), halfsign, y);
 }
 
 TEST_SIG (SV, F, 1, atanh, -1.0, 1.0)
